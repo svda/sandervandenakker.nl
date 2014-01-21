19b37ded3cc4a165d794930b159a28a62e11f909
Sander van den Akker <sander@myxt.nl>
2014-01-21 09:19:13 +0100

    Thingies.

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,1 @@
-ROOT_URL=http://www.sandervandenakker.nl forever /usr/local/bin/mrt -p 4000 --production
+ROOT_URL=http://www.sandervandenakker.nl forever /usr/local/bin/mrt -p 3000 --production --settings settings.json
