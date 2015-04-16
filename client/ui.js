4c658ee85dce7671239a9906155f1d7332106fba
Sander van den Akker <svdakker@gmail.com>
2015-04-16 20:38:13 +0700

    Implemented UI hooks.

--- client/ui.js
+++ client/ui.js
@@ -2,6 +2,11 @@ UI.hooks.subscribe('click_filter', function () {
   Template.layout.scrollToTop();
 });
 
+var lastRoute = null;
 UI.hooks.subscribe('router_route', function () {
-  Template.layout.scrollToTop();
+  var route = Router.current().route.getName();
+  if(lastRoute !== route) {
+    lastRoute = route;
+    Template.layout.scrollToTop(); //not working for feed more button (route)
+  }
 });
